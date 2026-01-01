32176a9308a60673afa747d236a910da10ad4018
Sergey B Kirpichev <skirpichev@gmail.com>
2026-01-01 10:42:31 +0300

    test RLIMIT_STACK on MacOS

--- a.c
+++ a.c
@@ -1,33 +1,27 @@
-#include <complex.h>
 #include <stdio.h>
-#if !defined(CMPLX)
-#define HAS_CMPLX 0
-static inline double complex
-CMPLX(double real, double imag)
+#include <stdlib.h>
+#include <sys/resource.h>
+int
+main(void)
 {
-    double complex z;
-    ((double *)(&z))[0] = real;
-    ((double *)(&z))[1] = imag;
-    return z;
-}
-#else
-#define HAS_CMPLX 1
-#endif
-int main(void)
-{
-    printf("CMPLX: %d\n", HAS_CMPLX);
-    double complex z, r;
-    z = CMPLX(4, 0);
-    r = csqrt(z);
-    printf("%20s csqrt(%+lf%+lf) (%+lf%+lf)\n", "CMPLX(4, 0)", creal(z), cimag(z), creal(r), cimag(r));
-    z = 4 + 0*_Complex_I;
-    r = csqrt(z);
-    printf("%20s csqrt(%+lf%+lf) (%+lf%+lf)\n", "4 + 0*_Complex_I", creal(z), cimag(z), creal(r), cimag(r));
-    z = CMPLX(-1, +0.001);
-    r = csqrt(z);
-    printf("%20s csqrt(%+lf%+lf) (%+lf%+lf)\n", "CMPLX(-1, +0.001)", creal(z), cimag(z), creal(r), cimag(r));
-    z = CMPLX(-1, -0.001);
-    r = csqrt(z);
-    printf("%20s csqrt(%+lf%+lf) (%+lf%+lf)\n", "CMPLX(-1, -0.001)", creal(z), cimag(z), creal(r), cimag(r));
+    struct rlimit new, old;
+
+    /* to trigger crash for GMP builds with alloca() enabled */
+    if (getrlimit(RLIMIT_STACK, &old)) {
+        perror("getrlimit");
+        return 1;
+    }
+    printf("old limits: %llu %llu\n", old.rlim_cur, old.rlim_max);
+    new.rlim_max = old.rlim_max;
+    new.rlim_cur = 128*1000;
+    if (setrlimit(RLIMIT_STACK, &new)) {
+        perror("setrlimit");
+        return 1;
+    }
+    if (getrlimit(RLIMIT_STACK, &old)) {
+        perror("getrlimit");
+        return 1;
+    }
+    printf("new limits: %llu %llu\n", old.rlim_cur, old.rlim_max);
     return 0;
 }
