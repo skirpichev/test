d2270900f571126f1f6adb4149da318fc5d659d5
Sergey B Kirpichev <skirpichev@gmail.com>
2026-03-25 01:41:18 +0300

    test __STDC_IEC_559__

--- a.c
+++ a.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/resource.h>
 int
 main(void)
 {
-    struct rlimit new, old;
-
-    /* to trigger crash for GMP builds with alloca() enabled */
-    if (getrlimit(RLIMIT_STACK, &old)) {
-        perror("getrlimit");
-        return 1;
-    }
-    printf("old limits: %llu %llu\n", old.rlim_cur, old.rlim_max);
-    new.rlim_max = old.rlim_max;
-    new.rlim_cur = 128*1000;
-    if (setrlimit(RLIMIT_STACK, &new)) {
-        perror("setrlimit");
-        return 1;
-    }
-    if (getrlimit(RLIMIT_STACK, &old)) {
-        perror("getrlimit");
-        return 1;
-    }
-    printf("new limits: %llu %llu\n", old.rlim_cur, old.rlim_max);
+    printf("__STDC_IEC_559__: %d\n", __STDC_IEC_559__);
     return 0;
 }
